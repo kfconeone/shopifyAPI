cc700dd8498e167bc28d732af14b97b8e8249692
kfconeone <kfconeone@gmail.com>
2022-04-13 17:24:31 +0800

    init4

--- api/login.ts
+++ api/login.ts
@@ -2,4 +2,5 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   console.log("現在時間:", new Date());
+  response.status(200).send("現在時間:" + new Date().toString());
 };
