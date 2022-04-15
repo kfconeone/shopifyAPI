6e6c523a7b9f07249b089fa3c9993e813d465c01
kfconeone <kfconeone@gmail.com>
2022-04-15 15:12:38 +0800

    add update

--- api/setProduct.ts
+++ api/setProduct.ts
@@ -1,6 +1,28 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+};
+
 export default async (request: VercelRequest, response: VercelResponse) => {
-  console.log("body:", request.body);
-  response.status(200).send(request.body);
+  // console.log("body:", request.body);
+  let product = JSON.parse(request.body);
+
+  product.variants.forEach((variant: any) => {
+    console.log(variant.price);
+    console.log(variant.sku);
+    console.log(variant.id);
+    console.log(product.handle);
+  });
+
+  response.status(200);
 };
