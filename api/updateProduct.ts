9f9a33b873fbe8976b2b655bfbd06276cbe69b29
kfconeone <kfconeone@gmail.com>
2022-04-15 16:59:46 +0800

    add update16

--- api/updateProduct.ts
+++ api/updateProduct.ts
@@ -1,6 +1,39 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import admin = require("firebase-admin");
 
-export default async (request: VercelRequest, response: VercelResponse) => {
-  console.log("body:", request.body);
-  response.status(200).send(request.body);
+const serviceAccount: admin.ServiceAccount = {
+  projectId: process.env.project_id,
+  clientEmail: process.env.client_email,
+  privateKey: process.env.private_key,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  let product = request.body;
+  let results: any = [];
+
+  product.variants.forEach((variant: any) => {
+    let temp = {
+      vid: "gid://shopify/ProductVariant/" + variant.id.toString(),
+      sku: variant.sku,
+      price: variant.price,
+      handle: product.handle,
+    };
+    results.push(temp);
+  });
+
+  console.log(results);
+  const db = admin.firestore();
+
+  results.forEach((result: any) => {
+    // console.log(result.vid.replace(/\//g, "").replace(":", ""));
+    db.collection("products")
+      .doc(result.vid.replace(/\//g, "").replace(":", ""))
+      .set(result);
+  });
+
+  response.status(200).send("Hello World!");
 };
