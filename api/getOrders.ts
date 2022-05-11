0268f8f83293f4a2ebe037f82a67761a868c119e
kfconeone <kfconeone@gmail.com>
2022-05-11 12:00:39 +0800

    update getOrders

---
+++ api/getOrders.ts
@@ -0,0 +1,47 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import admin = require("firebase-admin");
+
+const serviceAccount: admin.ServiceAccount = {
+  projectId: process.env.project_id,
+  clientEmail: process.env.client_email,
+  privateKey: process.env.private_key,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  let result = await getOrdersByDateRanges(
+    request.body.startDate,
+    request.body.endDate,
+    request.body.urlsuffixs
+  );
+  response.status(200).json(result);
+};
+
+async function getOrdersByDateRanges(
+  startDate: number,
+  endDate: number,
+  urlsuffixs: string[]
+) {
+  const db = admin.firestore();
+
+  let result: any = [];
+  let q = await db
+    .collection("orders")
+    .where("createdAt", ">=", startDate)
+    .where("createdAt", "<=", endDate)
+    .get();
+
+  let orders: any = [];
+  q.forEach((doc) => {
+    orders.push(doc.data());
+  });
+
+  orders = orders.filter((o: any) => {
+    return urlsuffixs.includes(o.urlsuffix);
+  });
+
+  return result;
+}
