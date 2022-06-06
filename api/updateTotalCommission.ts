a1d5147d04f2ace694ff9b7289e35478cb7b6a54
kfconeone <kfconeone@gmail.com>
2022-06-06 16:10:54 +0800

    updated updateMember9

---
+++ api/updateTotalCommission.ts
@@ -0,0 +1,91 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import admin = require("firebase-admin");
+import moment = require("moment");
+import _ from "lodash";
+
+const serviceAccount: admin.ServiceAccount = {
+  projectId: process.env.project_id,
+  clientEmail: process.env.client_email,
+  privateKey: process.env.private_key,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+export default async (request: VercelRequest, response: VercelResponse) => {
+  console.log(request.body);
+  try {
+    if (request.method == "POST") {
+      let currentMember: any = {};
+
+      let q = await db
+        .collection("members")
+        .where("urlsuffix", "==", request.body.urlsuffix)
+        .get();
+
+      q.forEach((doc: any) => {
+        currentMember["docId"] = doc.id;
+        currentMember = doc.data();
+      });
+
+      let currentDatetime = moment();
+
+      let startDate =
+        currentMember.lastCommissionDatetime == undefined
+          ? 0
+          : currentMember.lastCommissionDatetime;
+      let endDate = currentDatetime.add(-5, "minutes").valueOf();
+
+      if (moment(endDate).diff(startDate, "minutes") > 0) {
+        let totalOrderSumByDateRanges = await getAllMyOrderSumByDateRange(
+          startDate,
+          endDate,
+          request.body.urlsuffix
+        );
+
+        let totalCommission =
+          currentMember.totalCommission == undefined
+            ? 0
+            : currentMember.totalCommission;
+
+        totalCommission += totalOrderSumByDateRanges;
+        console.log(currentMember.docId);
+        db.collection("members").doc(currentMember.docId).update({
+          lastCommissionDatetime: endDate,
+          totalCommission: totalCommission,
+        });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  response.status(200).send("Hello World!");
+};
+
+async function getAllMyOrderSumByDateRange(
+  startDate: number,
+  endDate: number,
+  urlsuffix: string
+) {
+  let db = admin.firestore();
+  let q = await db
+    .collection("orders")
+    .where("createdAt", ">=", startDate)
+    .where("createdAt", "<=", endDate)
+    .get();
+
+  let orders: any[] = [];
+  q.forEach((doc) => {
+    orders.push(doc.data());
+  });
+
+  orders = orders
+    .filter((o) => {
+      return Object.keys(o.totalCommissions).includes(urlsuffix) && o.fullyPaid;
+    })
+    .map((o) => o.totalCommissions[urlsuffix]);
+
+  return _.sum(orders);
+}
